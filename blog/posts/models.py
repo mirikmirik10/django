126aaeb4936a9d711ddaabf48f24c9aac154ff58
Alexander Chaika <manti.by@gmail.com>
2021-08-25 15:53:37 +0300

    Update models

--- blog/posts/models.py
+++ blog/posts/models.py
@@ -1,10 +1,18 @@
-# models.py
+from django.conf import settings
 from django.db import models
 
 
 class Post(models.Model):
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
     image = models.ImageField(blank=True, null=True)
     slug = models.SlugField()
     text = models.TextField()
-    created_at = models.DateTimeField(auto_now_add=True, db_index=True)
+    created_at = models.DateTimeField(
+        auto_now_add=True, db_index=True
+    )
+
+
+class Tags(models.Model):
+    title = models.CharField(max_length=100)
+    publications = models.ManyToManyField(Post)
